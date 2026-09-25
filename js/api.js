13fe37c1f6951472a0f333f7c73eeec7a9e4537a
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2026-09-25 14:15:09 +0200

    Update API token in api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_TOKEN = "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";
+const API_TOKEN = "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";
 const API_URL = "https://api.github.com";
 
 export const getUser = async (userName) => {
